4a0cbe6439a65008bf35d541a6864118cb8b4f44
Tiju Baby <tiju.baby@infenox.com>
2024-07-02 13:33:00 +0530

    Create Employee Managument Application

---
+++ EmployeMS/EmployeMS/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmployeMS.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
